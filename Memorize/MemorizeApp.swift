f19df9afc1ea0ba0e00b592b17a50f0aab4b71ab
Kesh Ramjith <warpple@protonmail.com>
2022-02-03 23:04:24 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kesh Ramjith on 2022/02/03.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
